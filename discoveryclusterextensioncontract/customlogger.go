435c0a94eb767281205cd72343a8ba9b171a7e3d
rahasija <113361398+rahasija@users.noreply.github.com>
2023-03-26 17:49:50 +0530

    Adding customlogger to log (#6)

---
+++ discoveryclusterextensioncontract/customlogger.go
@@ -0,0 +1,65 @@
+package discoveryclusterextensioncontract
+
+import (
+	"context"
+	"strings"
+
+	discoveryclusterextensioncontract "github.com/Azure/AzureMigrate-discoveryclusterextension-event/discoveryclusterextensioncontract"
+	"github.com/go-logr/logr"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+type CustomLogger struct {
+	logr logr.Logger
+}
+
+func GetAzureLogger(ctx context.Context, annotationsMap map[string]string) CustomLogger {
+	c := CustomLogger{logr: log.FromContext(ctx)}
+	c.addAzureAnnotations(annotationsMap)
+
+	return c
+}
+
+// Adding annotation in custom logger.
+func (c *CustomLogger) addAzureAnnotations(annotationsMap map[string]string) {
+	for annotationkey, annotationvalue := range annotationsMap {
+		// only adding azure annotations
+		if strings.HasPrefix(annotationkey, "management.azure.com/") {
+			scrubbedKey := strings.TrimPrefix(annotationkey, "management.azure.com/")
+			// strcase.ToCamel() and serialize the system data.
+			c.logr = c.logr.WithValues(scrubbedKey, annotationvalue)
+		}
+	}
+}
+
+// Logging Exception Event
+func (c *CustomLogger) LogAgentException(ae discoveryclusterextensioncontract.AgentException) {
+
+	// todo: add message parameter into message, posssiblecauses, recommended action.
+	logger := c.logr.WithValues("MessageParameters", ae.MessageParameters)
+	logger = logger.WithValues("Message", ae.Message)
+	logger = logger.WithValues("PossibleCauses", ae.PossibleCauses)
+	logger = logger.WithValues("RecommendedAction", ae.RecommendedAction)
+	logger.Error(ae, "Agent Exception Logged.")
+}
+
+// Logging Telemetry Event
+func (c *CustomLogger) LogTelemetryEvent(te discoveryclusterextensioncontract.TelemetryEvent) {
+	logger := c.logr.WithValues("ServiceEventName", "TelemetryEvent")
+	logger = logger.WithValues("TelemetryEventId", te.TelemetryEventId)
+	logger = logger.WithValues("Dimensions", te.Dimensions)
+
+	logger.Info("TelemetryEvent")
+}
+
+// Logging Metric Event
+func (c *CustomLogger) LogMetricEvent(metricName string, metricValue int, metricDimensions map[string]string) {
+	logger := c.logr.WithValues("ServiceEventName", "MetricEvent")
+	logger = logger.WithValues("MetricValue", metricValue)
+
+	for metricDimensionKey, metricDimensionValue := range metricDimensions {
+		logger = logger.WithValues(metricDimensionKey, metricDimensionValue)
+	}
+
+	logger.Info(metricName)
+}
